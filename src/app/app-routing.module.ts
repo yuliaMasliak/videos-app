85de6a62acff85c7fbd9a696c35bdaf7acbd7211
Yulia Masliak <1985kozak@gmail.com>
2023-09-11 16:26:49 +0300

    feat: change router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,6 @@ const routes: Routes = [
     loadChildren: () =>
       import('./home/home.module').then((m) => m.HomePageModule)
   },
-
-  // {
-  //   path: '',
-  //   redirectTo: 'home',
-  //   pathMatch: 'full'
-  // },
   {
     path: '**',
     redirectTo: 'home/list',
