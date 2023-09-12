460da8e4e35fa9466c862f9bea0d549b17183697
Yulia Masliak <1985kozak@gmail.com>
2023-09-12 06:25:33 +0300

    feat: add dotenv

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import 'dotenv/config';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.log(err));
+
